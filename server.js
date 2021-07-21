1fe676bd59c7add9bbd37de32a3ef069dad78dac
mbrianna1170 <brianna.miller09@yahoo.com>
2021-07-21 10:56:56 -0500

    added express server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// // Get route
+// app.get('/', (req, res) => {
+//     res.json({
+//         message: 'Hello World!'
+//     });
+// });
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Default response for any other request (not found)
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
